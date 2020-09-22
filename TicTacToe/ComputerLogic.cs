714f05387c6e6c4523b2aaea550cebc1ab85ceaf
Jakeb Roos-Williams <jdrooswill@eagles.usi.edu>
2020-09-22 13:54:21 -0500

    Init commit

---
+++ TicTacToe/ComputerLogic.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TicTacToe
+{
+    class ComputerLogic
+    {
+        public static Holder determineAndPlaceMark(Holder[,] board)
+        {
+            Holder move = new Holder();
+            move.setValue(Board.O);
+
+            // See if middle is empty
+            if (board[1, 1].getValue() == Board.B)
+            {
+                move.setLocation(new System.Drawing.Point(1, 1));
+                return move;
+            }
+
+        // See if there are any open rows of two O's
+            // Check vertical combos
+            for (int x = 0; x < 3; x++)
+            {
+                if (board[x, 0].getValue() == Board.O && board[x, 1].getValue() == Board.O && board[x, 2].getValue() == Board.B)
+                {
+                    move.setLocation(new Point(x, 2));
+                    return move;
+                }
+            }
+
+            // checks across the board for possible rows of 3
+            for (int x = 0; x < 3; x++)
+            {
+                if (board[x, 0].getValue() == Board.B && board[x, 1].getValue() == Board.O && board[x, 2].getValue() == Board.O)
+                {
+                    move.setLocation(new Point(x, 0));
+                    return move;
+                }
+            }
+
+            // checks across the board for possible rows of 3
+            for (int x = 0; x < 3; x++)
+            {
+                if (board[x, 0].getValue() == Board.O && board[x, 1].getValue() == Board.B && board[x, 2].getValue() == Board.O)
+                {
+                    move.setLocation(new Point(x, 1));
+                    return move;
+                }
+            }
+
+
+            // Check horizontal combos
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[0, y].getValue() == Board.O && board[1, y].getValue() == Board.O && board[2, y].getValue() == Board.B)
+                {
+                    move.setLocation(new Point(2, y));
+                    return move;
+                }
+            }
+
+            // Check
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[0, y].getValue() == Board.B && board[1, y].getValue() == Board.O && board[2, y].getValue() == Board.O)
+                {
+                    move.setLocation(new Point(0, y));
+                    return move;
+                }
+            }
+
+            // Check
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[0, y].getValue() == Board.O && board[1, y].getValue() == Board.B && board[2, y].getValue() == Board.O)
+                {
+                    move.setLocation(new Point(1, y));
+                    return move;
+                }
+            }
+
+
+            // Check for diagonal combos
+
+            if (board[0,0].getValue() == Board.O && board[1,1].getValue() == Board.O && board[2,2].getValue() == Board.B)
+            {
+                move.setLocation(new Point (2, 2));
+                return move;
+            }
+                
+            else if (board[2, 0].getValue() == Board.O && board[1, 1].getValue() == Board.O && board[0, 2].getValue() == Board.B)
+            {
+                move.setLocation(new Point(0, 2));
+                return move;
+            }
+
+            // check
+            if (board[2,0].getValue() == Board.O && board[1,1].getValue() == Board.O && board[0,2].getValue() == Board.B)
+            {
+                move.setLocation(new Point (0, 2));
+                return move;
+            }
+            else if (board[2, 2].getValue() == Board.O && board[1, 1].getValue() == Board.O && board[0, 0].getValue() == Board.B)
+            {
+                move.setLocation(new Point(0, 0));
+                return move;
+            }
+
+            // check
+            if (board[0, 0].getValue() == Board.O && board[1, 1].getValue() == Board.B && board[2, 2].getValue() == Board.O)
+            {
+                move.setLocation(new Point(1, 1));
+                return move;
+            }
+            else if (board[0, 2].getValue() == Board.O && board[1, 1].getValue() == Board.B && board[2, 0].getValue() == Board.O)
+            {
+                move.setLocation(new Point(1, 1));
+                return move;
+            }
+
+            // ------------------ Check for defense ----------------------------
+
+            // See if there are any open rows of two O's
+            // Check vertical combos
+            for (int x = 0; x < 3; x++)
+            {
+                if (board[x, 0].getValue() == Board.X && board[x, 1].getValue() == Board.X && board[x, 2].getValue() == Board.B)
+                {
+                    move.setLocation(new Point(x, 2));
+                    return move;
+                }
+            }
+
+            // checks across the board for possible rows of 3
+            for (int x = 0; x < 3; x++)
+            {
+                if (board[x, 0].getValue() == Board.B && board[x, 1].getValue() == Board.X && board[x, 2].getValue() == Board.X)
+                {
+                    move.setLocation(new Point(x, 0));
+                    return move;
+                }
+            }
+
+            // checks across the board for possible rows of 3
+            for (int x = 0; x < 3; x++)
+            {
+                if (board[x, 0].getValue() == Board.X && board[x, 1].getValue() == Board.B && board[x, 2].getValue() == Board.X)
+                {
+                    move.setLocation(new Point(x, 1));
+                    return move;
+                }
+            }
+
+
+            // Check horizontal combos
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[0, y].getValue() == Board.O && board[1, y].getValue() == Board.O && board[2, y].getValue() == Board.B)
+                {
+                    move.setLocation(new Point(2, y));
+                    return move;
+                }
+            }
+
+            // Check
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[0, y].getValue() == Board.B && board[1, y].getValue() == Board.X && board[2, y].getValue() == Board.X)
+                {
+                    move.setLocation(new Point(0, y));
+                    return move;
+                }
+            }
+
+            // Check
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[0, y].getValue() == Board.X && board[1, y].getValue() == Board.B && board[2, y].getValue() == Board.X)
+                {
+                    move.setLocation(new Point(1, y));
+                    return move;
+                }
+            }
+
+
+            // Check for diagonal combos
+
+            if (board[0, 0].getValue() == Board.X && board[1, 1].getValue() == Board.X && board[2, 2].getValue() == Board.B)
+            {
+                move.setLocation(new Point(2, 2));
+                return move;
+            }
+
+            else if (board[2, 0].getValue() == Board.X && board[1, 1].getValue() == Board.X && board[0, 2].getValue() == Board.B)
+            {
+                move.setLocation(new Point(0, 2));
+                return move;
+            }
+
+            // check
+            if (board[2, 0].getValue() == Board.X && board[1, 1].getValue() == Board.X && board[0, 2].getValue() == Board.B)
+            {
+                move.setLocation(new Point(0, 2));
+                return move;
+            }
+            else if (board[2, 2].getValue() == Board.X && board[1, 1].getValue() == Board.X && board[0, 0].getValue() == Board.B)
+            {
+                move.setLocation(new Point(0, 0));
+                return move;
+            }
+
+            // check
+            if (board[0, 0].getValue() == Board.X && board[1, 1].getValue() == Board.B && board[2, 2].getValue() == Board.X)
+            {
+                move.setLocation(new Point(1, 1));
+                return move;
+            }
+            else if (board[0, 2].getValue() == Board.X && board[1, 1].getValue() == Board.B && board[2, 0].getValue() == Board.X)
+            {
+                move.setLocation(new Point(1, 1));
+                return move;
+            }
+
+            //
+            //
+            // Place any move on board
+            //
+            //
+
+            List<Holder> blankSpaces = new List<Holder>();
+            foreach(Holder h in board)
+            {
+                if(h.getValue() == Board.B)
+                {
+                    blankSpaces.Add(h);
+                }
+            }
+
+            move.setLocation(blankSpaces.ToArray()[0].getLocation());
+
+            return move;
+        }
+    }
+}
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public GFX engine;
+        public Board theBoard;
+        public static bool aiGame = false;
+
+        public static bool getAiGame()
+        {
+            return aiGame;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics toPass = panel1.CreateGraphics();
+            engine = new GFX(toPass);
+
+            theBoard = new Board();
+            theBoard.initBoard();
+
+            refreshLabel();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Point mouse = Cursor.Position;
+            mouse = panel1.PointToClient(mouse);
+            theBoard.detectHit(mouse);
+
+            refreshLabel();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            theBoard.reset();
+            GFX.setUpCanvas();
+        }
+
+        private void aboutButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi, Dr. Srishti! I hope you enjoy my tic-tac-toe game! \n\nHave an amazing summer! You're truly an amazing person and professor!");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void refreshLabel()
+        {
+            String newText = "It is ";
+            if(theBoard.getPlayerForTurn() == Board.X)
+            {
+                newText += "X";
+            }
+            else
+            {
+                newText += "O";
+            }
+            newText += "'s turn\n";
+            newText += "X has won " + theBoard.getXwins() + " times. \nO has won " + theBoard.getOwins() + " times." ;
+
+            label1.Text = newText;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Would you like to play against the computer?", "Tic Tac Toe", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                aiGame = true;
+            }
+        }
+    }
+}
